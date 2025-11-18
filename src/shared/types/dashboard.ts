2605bdf4e907f9b95a5edf9b750a9739748dff7d
whoysAndre <yellowcata2121@gmail.com>
2025-11-18 14:48:53 -0500

    Stats rendering dashboard finished

--- src/shared/types/dashboard.ts
+++ src/shared/types/dashboard.ts
@@ -5,10 +5,16 @@ export interface InventaryStats {
   productWithLowestStock:  ProductWithEstStock;
   productWithHighestStock: ProductWithEstStock;
   alerts:                  Alerts;
+  topClient: TopClient
 }
 
 export interface Alerts {
   lowStack: number;
+}
+
+export interface TopClient{
+  name: string;
+  total: number;
 }
 
 export interface ProductWithEstStock {
